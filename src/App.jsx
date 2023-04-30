f5aebac4d12bdb9b57a902751169d2518d4ea9d3
alexsandroferreira <ferreira.alex@outlook.com>
2023-04-30 13:56:33 -0300

    Class State

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { Component, useState } from 'react'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+class App extends React.Component {
+  // state com Class components
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [
+        {
+          id: 1,
+          title: 'Alexsandro',
+          body: 'Homem de 31 anos, Buscando se desenvolver em programação'
+        },
+        {
+          id: 2,
+          title: 'João',
+          body: 'Homem de 31 anos, Buscando se desenvolver em programação'
+        },
+        {
+          id: 3,
+          title: 'Carlos',
+          body: 'Homem de 31 anos, Buscando se desenvolver em programação'
+        }
+      ]
+    };
+  }
+
+  render() {
+    const { posts } = this.state;
+    return (
+    <div className='App'>
+      {posts.map(post => (
+        <div key={post.id}>
+          <h1>{post.title}</h1>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+    )
+  }
 }
 
-export default App
+export default App;
+// export default function App() {
+
+
+
+//   state = {
+//     posts: []
+//   }
+
+
+
+//   loadPosts = async () => {
+//     fetch('https://jsonplaceholder.typicode.com/posts')
+//       .then(reponse => Response.json())
+//       .then(posts => this.setState({ posts }))
+//   }
+
+//   return (
+
+
+//     <>
+//       <div>
+//         {posts.map(post => (
+//           <div key={post.id}>
+//             <h1>{post.title}</h1>
+//             <p>{post.body}</p>
+//           </div>
+//         ))}
+//       </div>
+
+//     </>
+
+//   )
+// }
+
+
